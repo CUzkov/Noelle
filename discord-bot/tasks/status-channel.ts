1017776efd80b12ba3adcddcd1b2fcd7aa64bf33
Daniil Uskov <uskovdanek@yandex-team.ru>
2023-05-28 18:42:47 +0300

    Fix paths

--- discord-bot/tasks/status-channel.ts
+++ discord-bot/tasks/status-channel.ts
@@ -9,30 +9,15 @@ export const startUpdateStatusChannel = async (client: Client) => {
     while (true) {
         await wait(5_000);
 
-        console.log('Update status channel starting');
-
         const ycInstanceConfig = await getSecret(Secrets.ycInstanceConfig);
-
-        console.log('Secret recived');
 
         const components: Components[] = [];
 
         for (let i = 0; i < ycInstanceConfig.length; i++) {
             const {ycInstanceId, mcServerName, mcServerPort, host} = ycInstanceConfig[i];
-
-            console.log(`Start building components for ${ycInstanceId}`);
-
             const {ycInstanceStatus, ycInstanceName} = await getYcInstanceInfo(ycInstanceId);
-
-            console.log(`Recived yc status for ${ycInstanceId}`);
-
             const mcServerInfo = await getMcServerStatus({host, serverPort: mcServerPort});
-
-            console.log(`Recived mc status for ${ycInstanceId}`);
-
             const mcServertimeLeftForRetryStart = await getMcServerTimeLeftToRetryStart({mcServerName});
-
-            console.log(`Recived mcServertimeLeftForRetryStart for ${ycInstanceId}`);
 
             components.push(
                 ...getServerCard({
@@ -48,11 +33,7 @@ export const startUpdateStatusChannel = async (client: Client) => {
             );
         }
 
-        console.log('Components builded');
-
         const channel = client.channels.cache.get(await getSecret(Secrets.discordStatusChannelId));
-
-        console.log('Channel getted');
 
         if (!channel || !channel.isTextBased()) {
             logger.fatal('Given status channel not exist or not text based');
@@ -60,8 +41,6 @@ export const startUpdateStatusChannel = async (client: Client) => {
         }
 
         const channelMessages = await channel.messages.fetch({limit: 100});
-
-        console.log('Channel messages getted');
 
         if (channelMessages.size !== components.length) {
             for (let i = 0; i < channelMessages.size; i++) {
@@ -73,7 +52,5 @@ export const startUpdateStatusChannel = async (client: Client) => {
         } else {
             await editMessagesComponents({messages: channelMessages, components});
         }
-
-        console.log('Finish updeting');
     }
 };
--- discord-bot/tasks/sync-mc-statistics.ts
+++ discord-bot/tasks/sync-mc-statistics.ts
@@ -15,15 +15,21 @@ export const syncMcStatistics = async () => {
             const localpath = `/home/cuzkov/${mcServerName.replace(/\s+/gi, '-').toLocaleLowerCase()}`;
 
             try {
+                console.log(mcServerStatsPath, existsSync(localpath));
+
                 if (!existsSync(localpath)) {
                     mkdirSync(localpath, {recursive: true});
                 }
+
+                console.log('start sync');
 
                 const client = await Client({
                     host,
                     username: login,
                     privateKey: Buffer.from(privateKey, 'base64').toString('utf-8'),
                 });
+
+                console.log('start download', mcServerStatsPath, localpath);
 
                 await client.downloadDir(mcServerStatsPath, localpath);
 
